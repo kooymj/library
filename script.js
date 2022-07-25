38837b0632a2afb8e1cf4af51bd83770144dfb93
John <kooymj@gmail.com>
2022-07-25 16:14:24 -0700

    create bookCard added to script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const addBookBtn = document.querySelector('#addBookBtn');
 const bookTitleInput = document.querySelector('#input-title');
 const bookPagesInput = document.querySelector('#input-pages');
+const libraryGrid = document.querySelector('.library');
+
 let myLibrary = [
     {
         title: 'Book1',
@@ -16,7 +18,38 @@ function Book(title, pages){
 function addBookToLibrary(){
     const newBook = new Book(bookTitleInput.value, bookPagesInput.value);
     myLibrary.push(newBook);
-    console.table(myLibrary);
+    createBookCard(newBook);
 }
 
 addBookBtn.addEventListener('click', addBookToLibrary);
+
+function createBookCard(book){
+    const bookCard = document.createElement('div');
+    const title = document.createElement('p');
+    const pages = document.createElement('p');
+    const readBtn = document.createElement('button');
+    const removeBtn = document.createElement('button');
+
+    bookCard.classList.add('card');
+
+    title.textContent = book.title;
+    pages.textContent = book.pages;
+    readBtn.textContent = 'READ';
+    removeBtn.textContent = 'REMOVE';
+
+    bookCard.appendChild(title);
+    bookCard.appendChild(pages);
+    bookCard.appendChild(readBtn);
+    bookCard.appendChild(removeBtn);
+    libraryGrid.appendChild(bookCard);
+}
+
+function loadBooks(library){
+    for(let book in library){
+        createBookCard(book);
+        console.table(book);
+    }
+}
+
+loadBooks(myLibrary);
+
